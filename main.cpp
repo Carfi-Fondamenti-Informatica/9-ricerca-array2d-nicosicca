d23ff65405c355c925ab982518d448ddb1f559c4
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-05-09 18:58:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include 'lib.h'
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int a;
+    char nomi[10][20];
+    char temp[20];
+
+    iniz(nomi,10);
+    iniz(temp,20);
+
+    for(int i=0; i<10; i++){
+        cin>>nomi[i];
+    }
+    cin>>temp;
+    a=ricerca(nomi,temp);
+    if(a!=-1)
+        cout<<a;
+    else
+        cout<<"non presente"<<endl;
+
+    return 0;
 }
