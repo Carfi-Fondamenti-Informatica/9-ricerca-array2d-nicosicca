498a1dc97ab989a8714d31fe84ae7a92925fffe6
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-05-09 18:58:49 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,34 @@
+void iniz(char x[][20], int a){
+    for(int i=0;i<a;i++)
+    {
+        for(int j=0;j<20;j++)
+        {
+            x[i][j]=' ';
+        }
+    }
+}
 
+void iniz(char x[], int a){
+    for(int i=0;i<a;i++)
+    {
+        x[i]=' ';
+    }
+}
+
+int ricerca(char nomi[10][20], char temp[20]){
+    int c;
+    for(int i=0;i<10;i++)
+    {
+        c=0;
+        for(int j=0;j<20;j++)
+        {
+            if(nomi[i][j]==temp[j])
+                c++;
+            else
+                break;
+        }
+        if(c==20)
+            return i;
+    }
+    return -1;
+}
